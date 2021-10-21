ff2f3df1c547da0a03be91b2cda5e93680ee64ff
willuwontu <willuwontu3@gmail.com>
2021-10-21 13:29:44 -0700

    Initial commit

---
+++ BlockForcePatch/BlockForcePatch.cs
@@ -0,0 +1,25 @@
+﻿using BepInEx;
+using HarmonyLib;
+using UnityEngine;
+using System;
+
+namespace BlockForcePatch
+{
+    // Declares our mod to Bepin
+    [BepInPlugin(ModId, ModName, Version)]
+    // The game our mod is associated with
+    [BepInProcess("Rounds.exe")]
+    public class BlockForcePatch : BaseUnityPlugin
+    {
+        private const string ModId = "com.willuwontu.rounds.BlockForcePatch";
+        private const string ModName = "Block Force Patch";
+        public const string Version = "1.0.0"; // What version are we on (major.minor.patch)?
+
+        void Awake()
+        {
+            // Use this to call any harmony patch files your mod may have
+            var harmony = new Harmony(ModId);
+            harmony.PatchAll();
+        }
+    }
+}
---
+++ BlockForcePatch/Patches/Block_Patch.cs
@@ -0,0 +1,53 @@
+﻿using HarmonyLib;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Reflection.Emit;
+
+namespace SimplyCards.Patches
+{
+    [HarmonyPatch]
+    class BlockForce_Patch
+    {
+
+        static Type GetNestedIDoBlockTransitionType()
+        {
+            var nestedTypes = typeof(Block).GetNestedTypes(BindingFlags.Instance | BindingFlags.NonPublic);
+            Type nestedType = null;
+
+            foreach (var type in nestedTypes)
+            {
+                if (type.Name.Contains("IDoBlock"))
+                {
+                    nestedType = type;
+                    break;
+                }
+            }
+
+            return nestedType;
+        }
+
+        static MethodBase TargetMethod()
+        {
+            return AccessTools.Method(GetNestedIDoBlockTransitionType(), "MoveNext");
+        }
+
+        static IEnumerable<CodeInstruction> Transpiler(IEnumerable<CodeInstruction> instructions)
+        {
+            var codes = new List<CodeInstruction>(instructions);
+
+            for (var i = 0; i < codes.Count; i++)
+            {
+                UnityEngine.Debug.Log($"{i}: {codes[i].opcode}, {codes[i].operand}");
+            }
+
+            codes[131] = new CodeInstruction(OpCodes.Ldc_R4, 10f);
+            codes[154] = new CodeInstruction(OpCodes.Ldc_R4, 10f);
+            codes[136] = new CodeInstruction(OpCodes.Ldc_I4_1);
+            codes[159] = new CodeInstruction(OpCodes.Ldc_I4_1);
+
+            return codes.AsEnumerable();
+        }
+    }
+}

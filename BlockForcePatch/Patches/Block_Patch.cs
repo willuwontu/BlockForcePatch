4baa7c93068be8107997f61481bab5591f99c828
willuwontu <willuwontu3@gmail.com>
2022-01-06 11:23:27 -0800

    Update Block_Patch.cs

--- BlockForcePatch/Patches/Block_Patch.cs
+++ BlockForcePatch/Patches/Block_Patch.cs
@@ -5,7 +5,7 @@ using System.Linq;
 using System.Reflection;
 using System.Reflection.Emit;
 
-namespace SimplyCards.Patches
+namespace BlockForcePatch.Patches
 {
     [HarmonyPatch]
     class BlockForce_Patch
